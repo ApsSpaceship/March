6a712af09646f6ddd55f09fd6016038d5d846224
jiyeon-agnes-lee <83017334+jiyeon-agnes-lee@users.noreply.github.com>
2022-03-18 23:59:59 +0900

    220318_solve : P2292_lee

--- 0322/BOJ/P2292/P2292_lee.java
+++ 0322/BOJ/P2292/P2292_lee.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 public class P2292 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-    // 입력받은 N
+    		// 입력받은 N
 		int N = sc.nextInt();
-    // 지나게 되는 방 개수
+    		// 지나게 되는 방 개수
 		int cnt = 1;
-    // 육각형 마다의 마지막 번호
+    		// 육각형 마다의 마지막 번호
 		int num = 1;
 		while (true) {
-      // 육각형의 마지막 번호보다 작거나 같으면 멈춘다.
+     		 	// 육각형의 마지막 번호보다 작거나 같으면 멈춘다.
 			if(num >= N)
 				break;
-      // 육각형의 마지막 번호는 6의 배수로 증가
+      			// 육각형의 마지막 번호는 6의 배수로 증가
 			num += 6 * cnt;
-      // 육각형 하나를 지나면 지나게 되는 방 개수 증가
+      			// 육각형 하나를 지나면 지나게 되는 방 개수 증가
 			cnt++;
 		}
-    // 
+    		// 출력
 		System.out.println(cnt);
 		sc.close();
 	}
