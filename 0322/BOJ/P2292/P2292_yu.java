0cf540bd03aaa70c25177d1e5714956fd7c58142
Yujiseul <98597009+Yujiseul@users.noreply.github.com>
2022-03-25 00:20:25 +0900

    220325_solve : P2775_yu
    220325_solve : P2775_yu

---
+++ 0322/BOJ/P2292/P2292_yu.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class bj_2292_벌집 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		// 최종 숫자
+		int num = sc.nextInt();
+
+		// 비교할 숫자
+		int n = 1;
+
+		// 횟수
+		int cnt = 1;
+
+		for (int i = 1; n < num; i++) {
+
+			n = n + (6 * i); // 규칙
+
+			cnt++;
+
+		}
+
+		// 출력
+		System.out.println(cnt);
+	}
+
+}
