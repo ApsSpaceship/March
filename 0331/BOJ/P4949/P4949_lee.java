fe24e57f76960a0b23b45faefda465a7e3c31182
jiyeon-agnes-lee <83017334+jiyeon-agnes-lee@users.noreply.github.com>
2022-03-31 01:42:59 +0900

    220331_notSolved : P4949_lee

---
+++ 0331/BOJ/P4949/P4949_lee.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class P4949 {
+	private static Stack<Character> stack;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> arr = new ArrayList<>();
+		while(true) {
+			String str = sc.nextLine();
+			if(str.equals(".")) break;
+			arr.add(str);
+		}
+		char[] cArr;
+		boolean flag = true;
+		for(int i =0; i < arr.size(); i++) {
+			stack = new Stack<>();
+			cArr = arr.get(i).toCharArray();
+			for(int j = 0; j < cArr.length; j++) {
+				char c = cArr[j];
+				switch(c) {
+				case '(' :
+					stack.add(c);
+					break;
+				case ')':
+					flag = remove(c);
+					break;
+				case '[':
+					stack.add(c);
+					break;
+				case ']':
+					flag = remove(c);
+					break;
+				}
+			}
+			if(!stack.empty()) flag = false;
+			if(flag) System.out.println("yes");
+			else System.out.println("no");
+			
+		}
+ 	}
+	public static boolean remove(char c) {
+		switch(c) {
+		case ')':
+			if(!stack.empty() && stack.peek() == '(') stack.pop();
+			else return false;
+			break;
+		case ']':
+			if(!stack.empty() && stack.peek() == '[') stack.pop();
+			else return false;
+			break;	
+		}	
+		return true;
+	}
+}
